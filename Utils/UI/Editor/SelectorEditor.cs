528531840787204d1d5d66b9b7933767ac2c450d
Theo Farnole <theo.farnole@gmail.com>
2019-06-11 19:05:37 +0200

    UI Extensions/Selector

---
+++ Utils/UI/Editor/SelectorEditor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+public class SelectorEditor
+{
+    [MenuItem("GameObject/UI Extensions/Selector", false, 1)]
+    public static void Foo(MenuCommand menuCommand)
+    {
+        var newGameObject = CreateSelector();
+
+        GameObjectUtility.SetParentAndAlign(newGameObject, menuCommand.context as GameObject);
+        Undo.RegisterCreatedObjectUndo(newGameObject, "Create " + newGameObject.name);
+        Selection.activeObject = newGameObject;
+        SceneView.FrameLastActiveSceneViewWithLock();
+    }
+
+    private static GameObject CreateSelector()
+    {
+        var go = new GameObject();
+
+        go.AddComponent<RectTransform>();
+        go.AddComponent<Selector>();
+        go.name = "Selector";
+
+        var label = new GameObject();
+        label.AddComponent<RectTransform>();
+        label.AddComponent<TextMeshProUGUI>();
+        label.name = "Label";
+
+        // parenting
+        label.transform.parent = go.transform;
+
+        return go;
+    }
+}
---
+++ Utils/UI/Selector.cs
@@ -0,0 +1,112 @@
+﻿using Erebos.Inputs;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class Selector : Selectable
+{
+    #region Fields
+
+    [Header("Selector Settings")]
+    [SerializeField] private List<string> _options = new List<string>();
+    [SerializeField] private TextMeshProUGUI _label;
+    [Space]
+    public UnityEvent onValueChanged;
+
+    private int _value = 0;
+
+    private bool _isHorizontalPressed = false;
+    private bool _isSelected = false;
+    #endregion
+
+    #region Fields
+    public int Value
+    {
+        get
+        {
+            return _value;
+        }
+
+        set
+        {
+            // clamp value
+            if (value < 0)
+            {
+                value = _options.Count - 1;
+            }
+            else if (value >= _options.Count)
+            {
+                value = 0;
+            }
+
+            // Invoke Event
+            if (_value != value)
+            {
+                onValueChanged?.Invoke();
+            }
+
+            _value = value;
+
+            UpdateLabel();
+        }
+    }
+    #endregion
+
+    #region MonoBehaviour Callbacks
+    protected override void Awake()
+    {
+        onValueChanged = new UnityEvent();
+        Value = 0;
+    }
+
+    void Update()
+    {
+        if (_isSelected)
+        {
+            Selected();
+        }
+    }
+    #endregion
+
+    #region Selectable Callbacks
+    public override void OnSelect(BaseEventData eventData)
+    {
+        base.OnSelect(eventData);
+        _isSelected = true;
+    }
+
+    public override void OnDeselect(BaseEventData eventData)
+    {
+        base.OnDeselect(eventData);
+        _isSelected = false;
+    }
+    #endregion
+
+    void Selected()
+    {
+        float horizontal = InputProxy.Character.Horizontal;
+        bool isHorizontalDown = (horizontal != 0) && (_isHorizontalPressed == false);
+
+        if (isHorizontalDown)
+        {
+            Value += (int)Mathf.Sign(horizontal);
+        }
+
+        _isHorizontalPressed = (horizontal != 0);
+    }
+
+    void UpdateLabel()
+    {
+
+        if (_options.Count > 1)
+        {
+            _label.text = _options[Value];
+        }
+    }
+}
