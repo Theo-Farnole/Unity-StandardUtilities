3b3cbda1b462311b33874e9ee511aea014b6565f
sdf <theo.farnole@gmail.com>
2019-07-13 00:26:24 +0200

    Add InverseClamp
    If int is greater than max, so int=max
    Else, if int is minor than min, so int=max

---
+++ Extensions/MyMath.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MyMath
+{
+    /// <summary>
+    /// If int is greater than max, so int=max
+    /// Else, if int is minor than min, so int=max 
+    /// </summary>
+    public static int InverseClamp(int i, int min, int max)
+    {
+        int value = i;
+
+        if (value > max)
+        {
+            value = min;
+        }
+
+        if (value < min)
+        {
+            value = max;
+        }
+
+        return value;
+    }
+}
