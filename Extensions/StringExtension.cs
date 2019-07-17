e257d80de3ba2508c61a6efcf0a56f6a9ba61371
sdf <theo.farnole@gmail.com>
2019-07-17 02:03:28 +0200

    [+] UrlToTexture method

--- Extensions/StringExtension.cs
+++ Extensions/StringExtension.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public static class StringExtension
 {
@@ -14,5 +16,35 @@ public static class StringExtension
         byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
         byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
         return new Color32(r, g, b, 255);
+    }
+
+    // code from
+    // https://stackoverflow.com/questions/31765518/how-to-load-an-image-from-url-with-unity
+    public static async Task<Texture2D> UrlToTexture(this string url)
+    {
+        using (UnityWebRequest www = UnityWebRequestTexture.GetTexture(url))
+        {
+            //begin requenst:
+            var asyncOp = www.SendWebRequest();
+
+            //await until it's done: 
+            while (asyncOp.isDone == false)
+            {
+                await Task.Delay(1000 / 30);//30 hertz
+            }
+
+            //read results:
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log($"{ www.error }, URL:{ www.url }");
+
+                return null;
+            }
+            else
+            {
+                //return valid results:
+                return DownloadHandlerTexture.GetContent(www);
+            }
+        }
     }
 }
