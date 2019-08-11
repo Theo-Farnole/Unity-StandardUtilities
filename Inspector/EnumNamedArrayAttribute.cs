b5f4a99e66dc301ebb8c2e1af132226daecde164
Theo <theo.farnole@gmail.com>
2019-08-11 15:46:04 +0200

    + DrawerEnumNamedArray
    code from https://answers.unity.com/questions/1589226/showing-an-array-with-enum-as-keys-in-the-property.html

---
+++ Inspector/EnumNamedArrayAttribute.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+/// <author>
+/// https://answers.unity.com/questions/1589226/showing-an-array-with-enum-as-keys-in-the-property.html
+/// </author>
+
+public class EnumNamedArrayAttribute : PropertyAttribute
+{
+    public string[] names;
+    public EnumNamedArrayAttribute(System.Type names_enum_type)
+    {
+        this.names = System.Enum.GetNames(names_enum_type);
+    }
+}
