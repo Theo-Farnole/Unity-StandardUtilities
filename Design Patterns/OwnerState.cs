4fd05503f72fa4499c7e45c344b8ac80ff15406d
Theo <theo.farnole@gmail.com>
2019-09-04 17:07:01 +0200

    Improve State pattern by adding a owner field

---
+++ Design Patterns/OwnerState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class OwnerState<T> : State
+{
+    protected T _owner;
+
+    public OwnerState(T owner) => _owner = owner;    
+}
