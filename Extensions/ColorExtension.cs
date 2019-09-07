b20c5d5d455ebad8bce11b5f5cca357d2171d410
Theo <theo.farnole@gmail.com>
2019-09-07 15:03:51 +0200

    Improve hex to color methods

---
+++ Extensions/ColorExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ColorExtension
+{
+    public static string ToHex(this Color c)
+    {
+        string hexR = ((int)(c.r * 255)).ToString("X2");
+        string hexG = ((int)(c.g * 255)).ToString("X2");
+        string hexB = ((int)(c.b * 255)).ToString("X2");
+
+        return hexR + hexG + hexB;
+    }
+}
