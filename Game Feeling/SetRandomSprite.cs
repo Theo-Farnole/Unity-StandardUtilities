0b4d7d0901fd71f62ddcc53b310a70e801d14f2a
Theo <theo.farnole@gmail.com>
2019-09-10 17:10:17 +0200

    Add SetRandomSprite script

---
+++ Game Feeling/SetRandomSprite.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SetRandomSprite : MonoBehaviour
+{
+    #region Fields
+    [SerializeField] private Sprite[] _sprites;
+    #endregion
+
+    #region Methods
+    void Awake()
+    {
+        int random = Random.Range(0, _sprites.Length);
+        GetComponent<SpriteRenderer>().sprite = _sprites[random];
+    }
+    #endregion
+}
