6c38d12265133794dae860145e4c0ad54737fb03
Theo <theo.farnole@gmail.com>
2019-09-21 14:52:43 +0200

    Set FixedTick Abstract to Virtual

--- Design Patterns/State.cs
+++ Design Patterns/State.cs
@@ -1,7 +1,7 @@
 public abstract class State
 {
     public abstract void Tick();
-    public abstract void FixedTick();
+    public virtual void FixedTick() { }
 
     public virtual void OnStateEnter() { }
     public virtual void OnStateExit() { }
