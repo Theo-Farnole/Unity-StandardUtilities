79f962bd5d7a1d1ecafdf5075f6e908bab1a7891
Theo <theo.farnole@gmail.com>
2019-10-16 14:22:31 +0200

    Remove ReadOnly drawer & add ShowOnly drawer

--- Editor & Debug/Inspector/ReadOnlyAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-// code from
-// https://answers.unity.com/questions/489942/how-to-make-a-readonly-property-in-inspector.html
-
-public class ReadOnlyAttribute : PropertyAttribute
-{
-
-}
