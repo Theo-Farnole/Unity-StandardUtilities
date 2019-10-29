563d26b1162da4c5462ee3bc1622586cb2abe5c0
Theo <theo.farnole@gmail.com>
2019-10-29 15:49:31 +0100

    Add Load method to SceneManagement

---
+++ SceneManagement/LoadingHandler.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingHandler : MonoBehaviour
+{
+    // it used only by SceneManagement
+}
--- SceneManagement/SceneManagement.cs
+++ SceneManagement/SceneManagement.cs
@@ -1,13 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 public static class SceneManagement
 {
+    #region Fields
     private static readonly string DATA_NAME = "Scene Management Data";
+
     private static SceneManagementData _data;
 
+    private static List<AsyncOperation> _asyncGameLogic = new List<AsyncOperation>();
+    private static List<AsyncOperation> _asyncUnload = new List<AsyncOperation>();
+
+    private static GameObject _loadingHandler; // owns loading coroutine
+    #endregion
+
+    #region Properties
     public static SceneManagementData Data
     {
         get
@@ -26,5 +36,87 @@ public static class SceneManagement
             return _data;
         }
     }
+    #endregion
+
+    #region Methods
+    #region Public Methods
+    public static void ReloadScene()
+    {
+        LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    /// <summary>
+    /// Load level scene with GameLogic
+    /// </summary>
+    public static void LoadScene(string level)
+    {
+        // async load LEVEL
+        SceneManager.LoadScene(level);
+
+        // async load GAME_LOGIC
+        _asyncGameLogic.Clear();
+
+        for (int i = 0; i < Data.GameLogicSceneName.Length; i++)
+        {
+            _asyncGameLogic.Add(SceneManager.LoadSceneAsync(_data.GameLogicSceneName[0], LoadSceneMode.Additive));
+            _asyncGameLogic[i].allowSceneActivation = false;
+        }
+
+        // start loading handler
+        CreateLoadingManager();
+        _loadingHandler.GetComponent<LoadingHandler>().StartCoroutine(LoadingHandler());
+    }
+    #endregion
+
+    #region Private Methods
+    static void CreateLoadingManager()
+    {
+        if (_loadingHandler != null)
+            return;
+
+        _loadingHandler = new GameObject();
+        _loadingHandler.AddComponent<LoadingHandler>();
+        UnityEngine.Object.DontDestroyOnLoad(_loadingHandler);
+    }
+
+    static IEnumerator LoadingHandler()
+    {
+        // Wait until the asynchronous scene fully loads
+        while (!IsLoadingCompleted())
+        {
+            yield return null;
+        }
+
+        AllowScenesActivation();
+    }
+
+    static bool IsLoadingCompleted()
+    {
+        //  check if game logic loaded
+        for (int i = 0; i < _asyncGameLogic.Count; i++)
+        {
+            if (_asyncGameLogic[i].progress < 0.9f)
+                return false;
+        }
+
+        return true;
+    }
+
+    static void AllowScenesActivation()
+    {
+        // load scenes
+        for (int i = 0; i < _asyncGameLogic.Count; i++)
+        {
+            _asyncGameLogic[i].allowSceneActivation = true;
+        }
+
+        // ... then unload old scenes
+        for (int i = 0; i < _asyncUnload.Count; i++)
+        {
+            _asyncUnload[i].allowSceneActivation = true;
+        }
+    }
+    #endregion
+    #endregion
 }
 
