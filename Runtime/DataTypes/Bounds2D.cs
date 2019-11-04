1395de65e7d9372a039a49df4afeb5d29ac9e2f3
Theo <theo.farnole@gmail.com>
2019-11-04 10:19:13 +0100

    Add DataTypes Bounds2D

---
+++ Runtime/DataTypes/Bounds2D.cs
@@ -0,0 +1,33 @@
+﻿namespace Utils
+{
+    using UnityEngine;
+
+    [System.Serializable]
+    public class Bounds2D
+    {
+        public float minimum;
+        public float maximum;
+
+        public Bounds2D(float min, float max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+
+        public Bounds2D(Vector2 limits)
+        {
+            minimum = limits.x;
+            maximum = limits.y;
+        }
+
+        public bool WithinLimits(float value)
+        {
+            return (value >= minimum && value <= maximum);
+        }
+
+        public Vector2 AsVector2()
+        {
+            return new Vector2(minimum, maximum);
+        }
+    }
+}
