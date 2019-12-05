55be20c849afab563547ecb719fd7a5c7648a06b
Theo <theo.farnole@gmail.com>
2019-12-05 19:35:50 +0100

    Add ExecuteAfterFrame method in MonoBehaviour extension

--- Runtime/Extensions/MonoBehaviourExtension.cs
+++ Runtime/Extensions/MonoBehaviourExtension.cs
@@ -6,22 +6,40 @@ using UnityEngine;
 public static class MonoBehaviourExtension
 {
     /// <summary>
-    /// Execute task after time.
+    /// Execute task after realtime.
     /// </summary>
     /// <param name="mono"></param>
     /// <param name="time"></param>
     /// <param name="task"></param>
     public static Coroutine ExecuteAfterTime(this MonoBehaviour mono, float time, Action task)
     {
-        return mono.StartCoroutine(Coroutine(time, task));
+        return mono.StartCoroutine(ExecuteAfterTimeCoroutine(time, task));
     }
 
     /// <summary>
     /// Called from ExecuteAfterTime.
     /// </summary>
-    static IEnumerator Coroutine(float time, Action task)
+    static IEnumerator ExecuteAfterTimeCoroutine(float time, Action task)
     {
         yield return new WaitForSecondsRealtime(time);
+
+        task();
+    }
+
+    /// <summary>
+    /// Execute task after one frame.
+    /// </summary>
+    /// <param name="mono"></param>
+    /// <param name="time"></param>
+    /// <param name="task"></param>
+    public static Coroutine ExecuteAfterFrame(this MonoBehaviour mono, Action task)
+    {
+        return mono.StartCoroutine(ExecuteAfterFrameCoroutine(task));
+    }
+
+    static IEnumerator ExecuteAfterFrameCoroutine(Action task)
+    {
+        yield return new WaitForEndOfFrame();
 
         task();
     }
